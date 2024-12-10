4d1bf7adea9d10c21698767212b7e7895bfc6121
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-12-10 23:59:15 +0530

    Plugin: nvim-neo-tree/neo-tree.nvim ( file explorer plugin for Neovim that provides a tree-style view of files, buffers, and git status, featuring integrated icons, customizable layouts, and multiple view sources)

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+        
+        require("neo-tree").setup({
+            window = {
+                width = 30,
+                auto_expand_width = false
+            }
+        })
+    end,
+}
