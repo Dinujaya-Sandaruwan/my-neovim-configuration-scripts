9f07ec5329e74875ab802036d739b401035e982f
Dinujaya Sandaruwan <88492493+Dinujaya-Sandaruwan@users.noreply.github.com>
2026-06-03 00:37:10 +0530

    Feature: Switch from tsserver to typescript-tools.nvim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,13 +16,32 @@ return {
   {
     "neovim/nvim-lspconfig",
     lazy = false,
+    dependencies = {
+      "pmizio/typescript-tools.nvim",
+      "nvim-lua/plenary.nvim",
+    },
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
+
+      -- Replace tsserver setup with typescript-tools
+      require("typescript-tools").setup({
+        capabilities = capabilities,
+        settings = {
+          -- Enable separate diagnostic server for better performance
+          separate_diagnostic_server = true,
+          -- Show diagnostics only when not in insert mode
+          publish_diagnostic_on = "insert_leave",
+          -- Typescript-specific settings
+          tsserver_file_preferences = {
+            importModuleSpecifierPreference = "relative",
+            includeInlayParameterNameHints = "all",
+            includeCompletionsForModuleExports = true,
+          },
+        }
       })
+
+      -- Other language servers remain the same
       lspconfig.solargraph.setup({
         capabilities = capabilities
       })
@@ -33,10 +52,14 @@ return {
         capabilities = capabilities
       })
 
+      -- Add these TypeScript-specific keymaps
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>tr", "<cmd>TSToolsRenameFile<cr>", {})
+      vim.keymap.set("n", "<leader>ti", "<cmd>TSToolsOrganizeImports<cr>", {})
+      vim.keymap.set("n", "<leader>tf", "<cmd>TSToolsFixAll<cr>", {})
     end,
   },
 }
